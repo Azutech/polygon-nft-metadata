4ff7dfcc2c5163768a2d970fa851dc15bc547465
Azutech <azunna.onugha@gmail.com>
2022-08-25 17:17:45 +0100

    the hardhat.config was updated

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -5,21 +5,21 @@ import dotenv from "dotenv"
 
 dotenv.config();
 
-// const TESTNET_RPC = process.env.TESTNET_RPC
-// const PRIVATE_KEY = process.env.PRIVATE_KEY as string
-// const POLYGONSCAN_API_KEY = process.env.POLYGONSCAN_API_KEY
+const TESTNET_RPC = process.env.TESTNET_RPC
+const PRIVATE_KEY = process.env.PRIVATE_KEY as string
+const POLYGONSCAN_API_KEY = process.env.POLYGONSCAN_API_KEY
 
 const config: HardhatUserConfig = {
   solidity: "0.8.9",
   networks: {
     mumbai: {
-      url: process.env.TESTNET_RPC,
-      accounts: [process.env.PRIVATE_KEY as string]
+      url: TESTNET_RPC,
+      accounts: [PRIVATE_KEY as string]
 
     },
   },
   etherscan: {
-   apiKey : process.env.POLYGONSCAN_API_KEY
+   apiKey : POLYGONSCAN_API_KEY
   }
 };
 
